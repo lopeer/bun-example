8a20b5329d4bbc2285fee878ab5fcd1798970f7d
Eze Sunday <mailsforeze@gmail.com>
2023-11-01 09:57:59 +0100

    first commit

---
+++ src/counter.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+import { describe, expect, test } from 'bun:test'
+
+
+describe('App', () => {
+  test('counter increments when button is clicked', async () => {
+    render(<App />)
+    const button = screen.getByText('count is 0');
+
+    fireEvent.click(button);
+
+    // Use waitFor to wait for the DOM to update
+    await waitFor(() => {
+      // Expect the counter to be 1
+      expect(screen.getByText('count is 1')).toBeTruthy()
+    });
+  });
+});
+
